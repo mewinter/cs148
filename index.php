1b8266eb7d02ee82a09ff204d1e77e3fae45594d
Meaghan Winter <mewinter@uvm.edu>
2015-09-25 11:25:31 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+
+        <h1> Meaghan Winter's CS 148 Assignments </h1>
+        <p><a href="tryme.php/">Tryme</a></p>
+
+        <h2> Assignment 1.0 Sample Tables</h2>
+        <ol><li><a href="assignment1.0/index.php">Home Page</a></li>
+            <li><a href="assignment1.0/tables.php">Tables </a></li>
+            <li><a href="assignment1.0/css/base.css">Style Sheet</a></li></ol>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
